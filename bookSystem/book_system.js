f236512de74455eedd9af4f0f0de787284842f87
MostafaHendy <hendymostafapc@gmail.com>
2026-06-02 15:34:03 -0400

    update delete code

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -26,7 +26,8 @@ function showbooks(){
     <p><strong>Book Author: </strong>${book.authorName}</p>
     <p><strong>Book Description: </strong>${book.bookDescription}</p>
     <p><strong>No. of Pages: </strong>${book.pagesNumber}</p>
-    <button onclick = "editbook(${index})">Edit</button>`);
+    <button onclick = "editbook(${index})">Edit</button>
+    <button onclick = "deletebook(${index})">Delete</button>`);
     document.getElementById("books").innerHTML = booksDiv.join("");
 }
 
@@ -45,4 +46,9 @@ function clearInputs(){
     document.getElementById("authorName").value="";
     document.getElementById("bookDescription").value="";
     document.getElementById("pagesNumber").value="";
+}
+
+function deletebook(index){
+    books.splice(index, 1);
+    showbooks();
 }
